092264ab9a6cc4a442c841d961a1ff58e0dc18c9
seu-usersaneme <seu-email>
2022-10-04 14:28:08 -0300

    added delete

--- src/app.js
+++ src/app.js
@@ -1,9 +1,32 @@
 import express from "express";
 import cors from 'cors'
+import multer from 'multer';
 import { PORT } from "./config.js";
 import { pool } from "./db.js"
 
 const app = express()
+
+const imgConfig = multer.diskStorage({
+    destination: (req, file, callback) => {
+        callback(null, "./uploads");
+    },
+    filename: (rea, file, callback) => {
+        callback(null, `image-${Date.now()}.${file.originalName}`)
+    }
+});
+
+const isImage = (req, file, callback) => {
+    if (file.mimetype.startsWith("images")) {
+        callback(null, true)
+    } else {
+        callback(null, Error("only image is allowed"))
+    }
+};
+
+const upload = multer({
+    storage: imgConfig,
+    fileFilter: isImage
+});
 
 app.use(express.json());
 app.use(cors({ origin: ['http://localhost:5173'], }))
@@ -32,11 +55,20 @@ app.get("/products", async (_req, res) => {
     res.json(row);
 })
 
-app.post("/create-product", async (req, res) => {
+app.post("/create-product", upload.single("image"), async (req, res) => {
     const { title, description, image } = req.body;
     const sql = 'INSERT INTO product (TITLE, DESCRIPTION, IMAGE) VALUES (?, ?, ?)';
     const result = await pool.query(sql, [title, description, image]);
     res.json(result);
+});
+
+app.delete("/delete-product/:id", (req, res) => {
+    const id = req.params.id;
+    const query = "DELETE FROM product WHERE id = ?"
+    pool.query(query, [id], (err, data) => {
+        if (err) return res.json(err);
+        res.json("Product has been removed successfully");
+    })
 });
 
 app.listen(PORT);
